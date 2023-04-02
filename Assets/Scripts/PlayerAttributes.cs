8de9d6748affcdb2c298ab78834f39789795cef9
AaronKennerly <kennerlyac@appstate.edu>
2023-04-02 15:06:47 -0400

    Done I guess???

--- Assets/Scripts/PlayerAttributes.cs
+++ Assets/Scripts/PlayerAttributes.cs
@@ -1,8 +1,13 @@
+// This project's movement mechanics were inspired by the youtuber Dawnosaur's open-source 2D platformer tutorial
+
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Player Attributes")] // Used to create new playerAttributes object
 public class PlayerAttributes : ScriptableObject
 {
+    /*
+    * These handle all linear movements
+    */
     [Header("Run")]
     public float runMaxSpeed; // speed in which acceleration ends
     public float runAccel; // how long until max speed is reach
@@ -14,6 +19,11 @@ public class PlayerAttributes : ScriptableObject
     [Range(0.01f, 1)] public float deccelInAir; // same as accelInAir but opposite
     public bool doConserveMomentum; // forfeit yourself to the rules of Yimir
 
+
+
+    /*
+    * These handle all calculations for gravity
+    */
     [Header("Gravity")]
     [HideInInspector] public float gStrength; // downward force of gravity
     [HideInInspector] public float gScale; // multiplier of player gravity
@@ -23,6 +33,11 @@ public class PlayerAttributes : ScriptableObject
     public float maxJumpSpeed; // upward terminal velocity
     [Space(20)]
 
+
+
+    /*
+    * These handle all calculations for jump
+    */
     [Header("Jump")]
     public float jHeight; // height of jump
     public float jTimeToMax; // length of time to reach max height after jumping
@@ -36,19 +51,29 @@ public class PlayerAttributes : ScriptableObject
     [Range(0.01f, 0.5f)] public float jBuffTime; // time before hitting ground where your jump still counts
     [Range(0.01f, 0.5f)] public float coyoteTime; // time after falling off ledge in which you can still jump
 
+
+
+    /*
+    *   These handle additional calculations for wall jumps
+    */ 
     [Header("Wall Jump")]
-    [Range(0f, 1.5f)]public float wallJTime;
-    public Vector2 wallJForce;
-    public bool turnOnWall;
+    [Range(0f, 1.5f)]public float wallJTime; // how long since last wall jump
+    public Vector2 wallJForce; // force in which you launch from wall
+    public bool turnOnWall; // cause player to turn on wall jump
 
     [Space(20)]
 
+
+
+    /*
+    * These handle all calculations for dash
+    */
     [Header("Dash")]
-    public int dashUses;
-    public int dashSpeed; 
+    public int dashUses; // how many dashes a player has available
+    public int dashSpeed;  // the velocity in which a dash propells the player
     public float dashSleepTime; // time for game freeze on dash
     [Space(5)]
-    public float dashAttackTime;
+    public float dashAttackTime; // how long for the attack phase of the dash
     [Space(5)]
     public float dashEndTime; // time after drag to idle
     public Vector2 dashEndSpeed; // slows down player after dash
@@ -56,7 +81,18 @@ public class PlayerAttributes : ScriptableObject
     [Space(5)]
     public float dashCD; // time before next use of dash
     [Space(5)]
-    [Range(0.01f, 0.5f)] public float dashInputBufferTime;
+    [Range(0.01f, 0.5f)] public float dashInputBufferTime; // how many times you can spam dash before next is qued
+
+    [Space(20)]
+
+
+
+    /*
+    *   These functions handle the slide movements
+    */
+    [Header("Slides")]
+    public float slideSpeed; // the speed in which the player slides down a wall
+    public float slideAccel; // the speed in which the player accelerates down a wall
 
 
 
--- Assets/Scripts/PlayerPhysics.cs
+++ Assets/Scripts/PlayerPhysics.cs
@@ -1,6 +1,10 @@
+// This project's movement mechanics were inspired by the youtuber Dawnosaur's open-source 2D platformer tutorial
+
 using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerPhysics : MonoBehaviour
 {
 
@@ -8,50 +12,61 @@ public class PlayerPhysics : MonoBehaviour
     public PlayerAttributes Attrib;
 
 
-    // handles all plater animations
+    /*
+    * This is where I'd put my player animations... IF I HAD ANY!!
+    */
     #region COMPONENTS
-    public Rigidbody2D rigid { get; private set; }
+    public Rigidbody2D rigid { get; private set; } // rigid 2D body that represents the player
     #endregion
 
-    // variables that control what actions players have
+    /*
+    * These control what actions the player(s) have available to them
+    */
     #region STATE PARAMETERS
 
-    public float lastOnGroundTimer { get; private set; }
-    public float lastOnWallTimer { get; private set; }
-    public float lastOnWallRightTimer { get; private set; }
-    public float lastOnWallLeftTimer {get; private set;}
-    public bool isJumping {get; private set;}
-    public bool isFacingRight {get; private set;} 
-    public bool isWallJumping {get; private set;}
-    public bool isDashing {get; private set;}
+    public float lastOnGroundTimer { get; private set; } // when player was last on the ground
+    public float lastOnWallTimer { get; private set; } // when player had last touched a wall
+    public float lastOnWallRightTimer { get; private set; } // when player had last touched right wall
+    public float lastOnWallLeftTimer {get; private set;} // when player had last touched left wall
+    public bool isJumping {get; private set;} // is player jumping?
+    public bool isFacingRight {get; private set;}  // is player facing right?
+    public bool isWallJumping {get; private set;} // is player wall jumping?
+    public bool isDashing {get; private set;} // is player dashing?
+    public bool isSlide {get; private set;} // is player sliding?
 
     // jump
-    private bool isPartialJump;
-    private bool isFalling;
-    private int jCount = 0;
-    private int wCount = 0;
-    private bool onGround;
+    private bool isPartialJump; // is player performing a partial jump (letting go of jump before achieving max height)
+    private bool isFalling; // is player falling?
+    private int jCount = 0; // how many times has player jumped before touching ground?
+    private int wCount = 0; // how many times has player wall jumped before touching ground?
+    private bool onGround; // is player on the ground?
+    private bool inLava; // is player in the lava?
     
     // wall jump
-    private float wallJumpTimer;
-    private int lastWallJumpD;
+    private float wallJumpTimer;  // when player last wall jumped
+    private int lastWallJumpD; // direction of last wall jump
 
     // Dash
-    private bool dashOnCD;
-    private Vector2 lastDashDir;
-    private bool isDashAttacking;
-    private int dashLeft;
+    private bool dashOnCD; // is player's dash on cooldown?
+    private Vector2 lastDashDir; // what was the direction of player's last dash?
+    private bool isDashAttacking; // is player commiting a dash attack?
+    private int dashLeft; // how many dashes does the player have left?
     #endregion
 
-    // controls basic movement
+    /*
+    * creates variables used to control input at a later stage
+    */ 
     #region INPUT PARAMETERS
     private Vector2 moveInput;
 
+    // keeps track of jump and dash times
     public float lastJumpTime {get; private set;}
     public float lastDashTime {get; private set;}
     #endregion
 
-    // utilizes inspection to 
+    /*
+    *   This creates check parameters to compare hitboxes with
+    */ 
     #region CHECK PARAMETERS
     [Header("Checks")]
     [SerializeField] private Transform groundCheckPoint;
@@ -60,8 +75,14 @@ public class PlayerPhysics : MonoBehaviour
     [SerializeField] private Transform frontWallCheckPoint;
 	[SerializeField] private Transform backWallCheckPoint;
     [SerializeField] private Vector2 wallCheckSize = new Vector2(0.5f,1f);
+    [Space(5)]
+    //[SerializeField] private Transform lavaCheckPoint;
+    //[SerializeField] private Vector2 lavaCheckSize = new (0.5f,1f);
     #endregion
 
+    /*
+    * These initialize the program. First Awake() then Start()
+    */
     #region LAYERS AND TAGS
     [Header("Layers and Tags")]
     [SerializeField] private LayerMask groundLayer;
@@ -79,7 +100,9 @@ public class PlayerPhysics : MonoBehaviour
 
     private void Update() {
             
-
+            /*
+            *   These keep track of the timers necessary to create fluid motion
+            */
             #region TIMERS
             lastOnGroundTimer -= Time.deltaTime;
             lastJumpTime -= Time.deltaTime;
@@ -89,22 +112,29 @@ public class PlayerPhysics : MonoBehaviour
             #endregion
 
 
+            /*
+            * This controls the control schemes for various movements
+            */
             #region INPUT HANDLER
             moveInput.x = Input.GetAxisRaw("Horizontal"); // gets direction of movement
             moveInput.y = Input.GetAxisRaw("Vertical"); // gets the jump
 
+            // move input
             if (moveInput.x != 0) {
                 CheckDirectionToFace(moveInput.x > 0);
             }
 
+            // jump input
             if(Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Jump")) {
                 OnJumpInput();
             }
             
+            // partial jump input
             if (Input.GetKeyUp(KeyCode.Space) || Input.GetButtonUp("Jump")) {
                 OnJumpUpInput();
             }
 
+            // dash input
             if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetAxis("Fire1") != 0) {
                 OnDashInput();
             }
@@ -113,6 +143,9 @@ public class PlayerPhysics : MonoBehaviour
             #endregion
 
 
+            /*
+            * This checks if the player is in contact with objects
+            */
             #region COLLISION CHECKS
             // ground check
             if (!isDashing && !isJumping) {
@@ -138,9 +171,16 @@ public class PlayerPhysics : MonoBehaviour
 
                 lastOnWallTimer = Mathf.Max(lastOnWallLeftTimer, lastOnWallRightTimer);
             }
+            //if(Physics2D.OverlapBox(lavaCheckPoint.position, lavaCheckSize, 0, groundLayer)) {
+              //  inLava = true;
+            //}
+
             #endregion
 
 
+            /*
+            * This checks if a jump is legitimate, if so, which one and what other actions surround it
+            */
             #region JUMP CHECKS
             // Checks if the player is jumping 
             if (isJumping && rigid.velocity.y < 0) {
@@ -172,7 +212,7 @@ public class PlayerPhysics : MonoBehaviour
                     isPartialJump = false;
                     isFalling = false;
                     Jump();
-                }
+                } // call walljump
                 else if (CanWallJump() && lastJumpTime > 0) {
                     isWallJumping = true;
                     isJumping = false;
@@ -192,17 +232,31 @@ public class PlayerPhysics : MonoBehaviour
 
             }
             #endregion
-            
+
+
+            /*
+            * This checks if a slide is legitimate
+            */
+            #region SLIDE CHECKS
+            // set it slide to true or false based on if the player
+            // is moving into the left or right wall
+            isSlide = (CanSlide() && (lastOnWallLeftTimer > 0 && moveInput.x < 0) 
+                || lastOnWallRightTimer > 0 && moveInput.x > 0);
+            #endregion
+
+
+            /*
+            * This checks if a dash is legitimate, if so, dash
+            */
             #region DASH CHECKS
             if (CanDash() && lastDashTime > 0) {
-                //freeze game for moment before dash
-                Sleep(Attrib.dashSleepTime);
+                Sleep(Attrib.dashSleepTime);  //freeze game for moment before dash
 
-                if (moveInput != Vector2.zero) {
-                    lastDashDir = moveInput;
+                if (moveInput != Vector2.zero) { // if player is moving
+                    lastDashDir = moveInput; // lastDirection = movement direction
                 } 
                 else {
-                    if (isFacingRight) {
+                    if (isFacingRight) {  // if player is facing right, set last direction right and vice versa
                         lastDashDir = Vector2.right;
                     } else {
                         lastDashDir = Vector2.left;
@@ -219,6 +273,10 @@ public class PlayerPhysics : MonoBehaviour
             }
             #endregion
 
+
+           /*
+           * This controls the force of gravity on the player(s)
+           */ 
             #region GRAVITY
             if(!isDashAttacking) {
                 if (isPartialJump) {
@@ -227,15 +285,22 @@ public class PlayerPhysics : MonoBehaviour
                     SetGScale(Attrib.gScale * Attrib.partialJumpGravMult);
                     // update velocity, keep y velocity lte max fall speed
                     rigid.velocity = new Vector2(rigid.velocity.x, Mathf.Max(rigid.velocity.y, -Attrib.maxFallSpeed));  
-                } else if ((isJumping || isWallJumping || isFalling) && Mathf.Abs(rigid.velocity.y) < Attrib.jHangTime) {
+                }
+                else if (isSlide) {
+                    // set the gravity scale to 0 
+                    SetGScale(0);
+                }
+                 else if ((isJumping || isWallJumping || isFalling) && Mathf.Abs(rigid.velocity.y) < Attrib.jHangTime) {
                     SetGScale(Attrib.gScale * Attrib.jHangGravMult);
-                } else if (rigid.velocity.y < 0) {
+                }
+                 else if (rigid.velocity.y < 0) {
                     // higher grav when falling
                     SetGScale(Attrib.gScale * Attrib.fallGravMult);
                     // caps fall speed
                     rigid.velocity = new Vector2(rigid.velocity.x, 
                         Mathf.Max(rigid.velocity.y, -Attrib.maxFallSpeed));
-                } else {
+                }
+                 else {
                     // default grav
                     SetGScale(Attrib.gScale);
                 }
@@ -253,16 +318,26 @@ public class PlayerPhysics : MonoBehaviour
         else if (isDashAttacking) {
             Run(Attrib.dashEndRunLerp);
         }
+
+        // if slide 
+        if (isSlide) {
+            Slide();
+        }
     }
 
-    // movement
+
+    /*
+    * These functions determine all basic movement paterns
+    */
     #region RUN METHODS
     private void Run(float lerpNum)
     {
         
         float targetSpeed = moveInput.x * Attrib.runMaxSpeed; // calculate direction and velocity
 
-
+        /*
+        * This calculates the rate of acceleration
+        */
         #region Calculate AccelRate
         float accelRate;
 
@@ -287,6 +362,10 @@ public class PlayerPhysics : MonoBehaviour
         }
         #endregion
 
+
+        /*
+        *  This allows for a more fluid feeling movement rather than linear movement
+        */
         #region Conserve Momentum
         if (Attrib.doConserveMomentum && Mathf.Abs(rigid.velocity.x) > Mathf.Abs(targetSpeed) && 
             Mathf.Sign(rigid.velocity.x) == Mathf.Sign(targetSpeed) && 
@@ -309,16 +388,22 @@ public class PlayerPhysics : MonoBehaviour
     #endregion
 
 
+    /*
+    *   General methods that didn't find a place amongst a specific movement
+    */
     #region GEN METHODS
     
+    // set gravity with new scale
     public void SetGScale(float scale) {
         rigid.gravityScale = scale;
     }
 
+    // call perform sleep for given time
     private void Sleep(float time) {
         StartCoroutine(nameof(PerformSleep), time);
     }
 
+    // pause player for brief period before dash
     private IEnumerator PerformSleep(float time) {
         Time.timeScale = 0;
         yield return new WaitForSecondsRealtime(time); 
@@ -327,6 +412,9 @@ public class PlayerPhysics : MonoBehaviour
     #endregion
 
 
+    /*
+    * These are where the checks occur that determine the legitimacy of actions
+    */
     #region  CHECK FUNCTIONS
 
     // This checks the direction of the player
@@ -337,17 +425,22 @@ public class PlayerPhysics : MonoBehaviour
         }
     }
 
+    // check if player can jump
     private bool CanJump() {
         return (lastOnGroundTimer > 0 && !isJumping) || jCount < 2;
     }
 
+    // check if player can do partial jump
     private bool CanPartialJump() {
         return isJumping && rigid.velocity.y > 0;
     }
 
+    // check if player can wall partial jump
     private bool CanWallPJump() {
         return isWallJumping && rigid.velocity.y > 0;
     }
+
+    // check if player can dash
     private bool CanDash() {
         if (!isDashing && dashLeft < Attrib.dashUses &&lastOnGroundTimer > 0 && !dashOnCD) {
             StartCoroutine(nameof(RefillDash), 1);
@@ -356,12 +449,20 @@ public class PlayerPhysics : MonoBehaviour
         return dashLeft > 0;
     }
     
+    // check if player can wall jump
      private bool CanWallJump() {
         
             return onGround && lastOnGroundTimer <= 0 && lastOnWallTimer > 0 &&
             lastJumpTime > 0 && wCount < 1 && (!isWallJumping ||
             (lastOnWallRightTimer > 0 && lastWallJumpD == 1) || 
             (lastOnWallLeftTimer > 0 && lastWallJumpD == -1));
+    }
+    
+    // check if player can slide
+    public bool CanSlide()
+    {
+        return (lastOnWallTimer > 0 && !isJumping && !isWallJumping 
+                    && !isDashing && lastOnGroundTimer <= 0);
     }
     #endregion
 
@@ -377,6 +478,9 @@ public class PlayerPhysics : MonoBehaviour
     }
 
 
+    /*
+    * All of the functions that controls jump
+    */
     #region JUMP FUNCTIONS
     private void Jump() {
 
@@ -386,6 +490,9 @@ public class PlayerPhysics : MonoBehaviour
         lastOnGroundTimer = 0;
         
 
+        /*
+        * This is where jump is calculated
+        */
         #region Jump
         // calculate the the jump force
         float force = Attrib.jForce;
@@ -418,6 +525,10 @@ public class PlayerPhysics : MonoBehaviour
         lastOnWallLeftTimer = 0;
         lastOnWallRightTimer = 0;
 
+
+        /*
+        * This is where the wall jump occures
+        */
         #region WALL JUMP
         Vector2 force = new Vector2(Attrib.wallJForce.x, Attrib.wallJForce.y);
         
@@ -442,35 +553,57 @@ public class PlayerPhysics : MonoBehaviour
     }
     #endregion
 
-    
+
+    /*
+    * This is where slide is calculated
+    */
+    #region SLIDE METHOD
+    private void Slide() {
+        // like run but on the y axis 
+        float speed = Attrib.slideSpeed - rigid.velocity.y;
+        float force = speed * Attrib.slideAccel;
+
+        // clamp the force 
+        force = Mathf.Clamp(force, -Mathf.Abs(speed) * 
+            (1 / Time.fixedDeltaTime), Mathf.Abs(speed) * 
+            (1 / Time.fixedDeltaTime));
+
+        rigid.AddForce(force * Vector2.up);
+    }
+    #endregion
+
+
+    /*
+    * These are all the methods that calculate dash
+    */ 
     #region DASH METHODS
     // dash coroutine
     private IEnumerator StartDash(Vector2 dir) {
-       lastOnGroundTimer = 0;
+       lastOnGroundTimer = 0; // set necessary values to 0
        lastDashTime = 0;
        
 
-       float startTime = Time.time;
+       float startTime = Time.time; // set start time
 
-       dashLeft--;
-       isDashAttacking = true;
-       SetGScale(0);
+       dashLeft--; // remove dash
+       isDashAttacking = true; // activate dash attack
+       SetGScale(0); // remove gravity
 
-       while (Time.time - startTime <= Attrib.dashAttackTime) {
-            rigid.velocity = dir.normalized * Attrib.dashSpeed;
+       while (Time.time - startTime <= Attrib.dashAttackTime) { // during attack phase
+            rigid.velocity = dir.normalized * Attrib.dashSpeed;  // set velocity
             // pauses loop until next frame
             yield return null;
        }
 
-       startTime = Time.time;
-       isDashAttacking = false;
+       startTime = Time.time;  // set new start time
+       isDashAttacking = false; // end attack phase
 
        // begin end of dash
-       SetGScale(Attrib.gScale);
-       rigid.velocity = Attrib.dashEndSpeed * dir.normalized;
+       SetGScale(Attrib.gScale); // return to normal gravity
+       rigid.velocity = Attrib.dashEndSpeed * dir.normalized; // set new velocity
 
-       while (Time.time - startTime <= Attrib.dashEndTime) {
-            yield return null;
+       while (Time.time - startTime <= Attrib.dashEndTime) { // during end phase
+            yield return null; // do nothing
        }
 
        // dash over
@@ -478,27 +611,31 @@ public class PlayerPhysics : MonoBehaviour
     }
 
     private IEnumerator RefillDash(int dashes) {
-        dashOnCD = true;
-        yield return new WaitForSeconds(Attrib.dashCD);
-        dashOnCD = false;
-        dashLeft++;
+        dashOnCD = true; // set cooldown
+        yield return new WaitForSeconds(Attrib.dashCD); // wait for cooldown
+        dashOnCD = false; // finish cooldown
+        dashLeft++; // award dash
     }
     #endregion
 
 
+    /*
+    * These check major inputs
+    */
     #region INPUT CHECKS
-    // these handle input detected in Update()
     public void OnJumpInput() {
             // set 
             lastJumpTime = Attrib.jBuffTime;
     }
 
+    // check if partial jump and sey isPartialJump if so
     public void OnJumpUpInput() {
         if (CanPartialJump()) {
             isPartialJump = true;
         }
     }
 
+    
     public void OnDashInput() {
         lastDashTime = Attrib.dashInputBufferTime;
     }
