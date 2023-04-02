840420a2f0a6ff528dfa9fdae7ac4dbbcb0dff7c
AaronKennerly <kennerlyac@appstate.edu>
2023-04-02 00:34:00 -0400

    Taks 1 donegit status

--- Assets/Scripts/PlayerAttributes.cs
+++ Assets/Scripts/PlayerAttributes.cs
@@ -14,16 +14,26 @@ public class PlayerAttributes : ScriptableObject
     [Range(0.01f, 1)] public float deccelInAir; // same as accelInAir but opposite
     public bool doConserveMomentum; // forfeit yourself to the rules of Yimir
 
+    [Header("Gravity")]
+    [HideInInspector] public float gStrength; // downward force of gravity
+    [HideInInspector] public float gScale; // multiplier of player gravity
+    [Space(5)]
+    public float fallGravMult; // gravity multiplier when falling
+    public float maxFallSpeed; // downward terminal velocity
+    [Space(20)]
+
     [Header("Jump")]
     public float jHeight; // height of jump
     public float jTimeToMax; // length of time to reach max height after jumping
-    [HideInInspector] public float jforce; // force applied on jump
+    [HideInInspector] public float jForce; // force applied on jump
     public float partialJumpGravMult; // gravity multiplier when player releases jump
     [Range(0f, 1)] public float jHangGravMult; // reduces gravity when near max height
     public float jHangTime; // time in which player is in hang time
-    [space(5)]
+    [Space(5)]
     public float jHangAccel; // acceleration in hang time
     public float jHangMaxSpeed; // max speed in hang time
+    [Range(0.01f, 0.5f)] public float jBuffTime;
+    [Range(0.01f, 0.5f)] public float coyoteTime;
     
      
     private void OnValidate()
@@ -32,6 +42,15 @@ public class PlayerAttributes : ScriptableObject
         runAccelVal = (50 * runAccel) / runMaxSpeed;
         runDeccelVal = (50 * runDeccel) / runMaxSpeed;
         
+        // calculate strength of gravity
+        gStrength = -(2 * jHeight) / (jTimeToMax * jTimeToMax);
+
+        // scale of gravity on rigid body
+        gScale = gStrength / Physics2D.gravity.y;
+
+        // calculate upward force of jump
+        jForce = Mathf.Abs(gStrength) * jTimeToMax;
+         
 
         // Clamp takes the parameters (value, min, max) as input and returns result between min and max
         #region Variable Ranges
